19f014e8a6e9d645fa39f94ecf81f54c0f55c223
Name <user@example.com>
2024-06-22 14:14:14 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'digital-banking-web';
+  constructor(private authService : AuthService) {
+  }
+  ngOnInit() {
+    this.authService.loadJwtTokenFromLocalStorage();
+  }
 }
